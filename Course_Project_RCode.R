6a8cd14080377917f0ef4bdff6766f5441a0ddad
kedarrangnekar <ksrangnekar@gmail.com>
2015-09-28 04:08:20 +0530

    Main Code

---
+++ Course_Project_RCode.R
@@ -0,0 +1,10 @@
+setwd("E:\\3_DATA SCIENCE SPECIALISATION\\GETTING AND CLEANING DATA\\Assignments\\Week3")
+
+#### File Download ####
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(url = url , destfile="./wearable.zip")
+
+### Unzipping Files ####
+unzip("./wearable.zip")
+
+source(run_analysis.R)
