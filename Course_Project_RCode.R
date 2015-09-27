1b6217a933c55b930b3eba6f24721fb56f88ea30
kedarrangnekar <ksrangnekar@gmail.com>
2015-09-28 04:40:13 +0530

    Update Course_Project_RCode.R

--- Course_Project_RCode.R
+++ Course_Project_RCode.R
@@ -4,7 +4,7 @@ setwd("E:\\3_DATA SCIENCE SPECIALISATION\\GETTING AND CLEANING DATA\\Assignments
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(url = url , destfile="./wearable.zip")
 
-### Unzipping Files ####
-unzip("./wearable.zip")
+### Unzipping Files and storing in a folder "wearable ####
+
 
 source(run_analysis.R)
