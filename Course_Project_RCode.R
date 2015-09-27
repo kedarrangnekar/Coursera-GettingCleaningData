05c89a196de722121d6b8fb947aac8ac96f8e4e6
kedarrangnekar <ksrangnekar@gmail.com>
2015-09-28 04:41:19 +0530

    Adding Main Code

--- Course_Project_RCode.R
+++ Course_Project_RCode.R
@@ -1,6 +1,6 @@
 setwd("E:\\3_DATA SCIENCE SPECIALISATION\\GETTING AND CLEANING DATA\\Assignments\\Week3")
 
-#### File Download ####
+#### File Download #####
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(url = url , destfile="./wearable.zip")
 
